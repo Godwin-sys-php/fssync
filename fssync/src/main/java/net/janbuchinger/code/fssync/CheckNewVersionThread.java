b6e27d140ead906f01151caad21b344a9b0722d8
Jan Buchinger <janbuchinger@protonmail.ch>
2018-09-29 20:51:35 +0200

    improved checking, updated URL to check and commented

--- fssync/src/main/java/net/janbuchinger/code/fssync/CheckNewVersionThread.java
+++ fssync/src/main/java/net/janbuchinger/code/fssync/CheckNewVersionThread.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Jan Buchinger
+ * Copyright 2017-2018 Jan Buchinger
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,35 +15,75 @@
  */
 package net.janbuchinger.code.fssync;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.nio.charset.Charset;
 
 import javax.swing.SwingUtilities;
 
 import org.apache.commons.io.IOUtils;
 
-public class RunCheckForNewVersion implements Runnable {
+/**
+ * This <code>Runnable</code> is executed after the main UI is built and shown
+ * and the setting to ignore the new version dialog is false.
+ * 
+ * @author Jan Buchinger
+ *
+ */
+public class CheckNewVersionThread implements Runnable {
+	/**
+	 * The current version string
+	 */
 	private final String version;
+	/**
+	 * The main UI class
+	 */
 	private final FSSyncUI ui;
 
-	public RunCheckForNewVersion(String version, FSSyncUI ui) {
+	/**
+	 * Constructs a new <code>CheckNewVersionThread</code>
+	 * 
+	 * @param version
+	 *            The program executable version
+	 * 
+	 * @param ui
+	 *            The main UI object.
+	 */
+	public CheckNewVersionThread(String version, FSSyncUI ui) {
 		this.version = version;
 		this.ui = ui;
 	}
 
+	/**
+	 * Checks if a new version of FSSync is available. If yes a dialog is shown to
+	 * inform the user.
+	 */
 	@Override
 	public void run() {
 		try {
-			URL url = new URL("http://code.janbuchinger.net/fssync/currentVersion");
-			String currentVersion = IOUtils.toString(url.openStream(), Charset.forName("UTF-8"));
-			if (!currentVersion.trim().equals(version)) {
-				SwingUtilities.invokeLater(new RunNotifyNewVersion(ui, currentVersion));
+			try {
+				// the URL where the last published version is stored as text
+				URL url = new URL("https://code.janbuchinger.net/fssync/currentVersion");
+
+				// try to read the version file from the web server.
+				String currentVersion = IOUtils.toString(url.openStream(), Charset.forName("UTF-8")).trim();
+				// if a string was read,
+				if (currentVersion != null) {
+					// if the read string length is longer than zero
+					if (currentVersion.length() > 0) {
+						// and if the current binary version does not equal the online version
+						if (!currentVersion.equals(version)) {
+							// then notify the user about the new version
+							SwingUtilities.invokeLater(new RunNotifyNewVersion(ui, currentVersion));
+						}
+					}
+				}
+			} catch (UnknownHostException e) {
+				// if there is no Internet connection...
+				System.err.println(
+						"could not retrieve current version from the web server; ip could not be resolved");
 			}
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
