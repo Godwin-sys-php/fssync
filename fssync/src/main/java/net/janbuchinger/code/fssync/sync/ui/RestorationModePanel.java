fb2919c8b55d9b40eb1d5f5205a658aa7b593a11
Jan Buchinger <janbuchinger@protonmail.ch>
2018-09-29 18:34:37 +0200

    made the restoration mode choice panel prettier and safer

--- fssync/src/main/java/net/janbuchinger/code/fssync/sync/ui/RestorationModePanel.java
+++ fssync/src/main/java/net/janbuchinger/code/fssync/sync/ui/RestorationModePanel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Jan Buchinger
+ * Copyright 2017-2018 Jan Buchinger
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,8 +15,11 @@
  */
 package net.janbuchinger.code.fssync.sync.ui;
 
+import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JCheckBox;
@@ -29,37 +32,68 @@ import javax.swing.JSeparator;
 import net.janbuchinger.code.mishmash.ui.UIFx;
 
 @SuppressWarnings("serial")
-public class RestorationModePanel extends JPanel {
+public class RestorationModePanel extends JPanel implements ActionListener {
 
-	private final JRadioButton rbRestoreDamaged, rbRestoreModified, rbRestoreAll;
+	private final JRadioButton rbRestoreSoft, rbUndoChanges, rbRestoreAll;
 	private final JCheckBox ckDeleteNew;
-	public final static int RESTORE_ALL = 0, RESTORE_MODIFIED = 1, RESTORE_DAMAGED = 2;
+	public final static int MODE_SOFT = 1, MODE_ALL = 2, MODE_UNDO_CHANGES = 3;
 
 	public RestorationModePanel(JDialog dialog) {
-		ButtonGroup bg = new ButtonGroup();
-		rbRestoreDamaged = new JRadioButton("Beschädigte Wiederherstellen");
-		bg.add(rbRestoreDamaged);
-		rbRestoreModified = new JRadioButton("Geänderte Wiederherstellen");
-		bg.add(rbRestoreModified);
-		rbRestoreAll = new JRadioButton("Alle Wiederherstellen");
-		bg.add(rbRestoreAll);
+		Color green = Color.GREEN.darker();
+		Color red = Color.RED.darker();
 
-		rbRestoreModified.setSelected(true);
+		ButtonGroup bg = new ButtonGroup();
+		rbRestoreSoft = new JRadioButton("Sanft Wiederherstellen");
+		rbRestoreSoft.setForeground(green);
+		bg.add(rbRestoreSoft);
+		rbUndoChanges = new JRadioButton("Änderungen rückgängig Machen");
+		rbUndoChanges.setForeground(red);
+		bg.add(rbUndoChanges);
+		rbRestoreAll = new JRadioButton("Alle Wiederherstellen");
+		rbRestoreAll.setForeground(red);
+		bg.add(rbRestoreAll);
+		JLabel lbRestoreSoft = new JLabel("Nur Dateien die im Quellordner fehlen Wiederherstellen");
+		lbRestoreSoft.setForeground(green);
+		JLabel lbRestoreAll = new JLabel(
+				"Es werden alle Dateien aus dem Zielverzeichnis in das Quellverzeichnis kopiert");
+		lbRestoreAll.setForeground(red);
+		JLabel lbUndoChanges = new JLabel(
+				"Alle geänderten Dateien mit ihrer alten Version aus dem Zielverzeichnis Überschreiben");
+		lbUndoChanges.setForeground(red);
+
+		JLabel lbTitle = new JLabel("Modus zum Wiederherstellen:");
+		lbTitle.setForeground(Color.CYAN.darker());
+
+		rbRestoreSoft.addActionListener(this);
+		rbRestoreAll.addActionListener(this);
+		rbUndoChanges.addActionListener(this);
+
+		rbRestoreSoft.setSelected(true);
 
 		ckDeleteNew = new JCheckBox("Neue Dateien Löschen");
+		ckDeleteNew.setEnabled(false);
+		ckDeleteNew.setForeground(red);
 
 		setLayout(new GridBagLayout());
 		GridBagConstraints c = UIFx.initGridBagConstraints();
 
-		add(new JLabel("Modus zum Wiederherstellen:"), c);
+		add(lbTitle, c);
 		c.gridy++;
-		add(rbRestoreDamaged, c);
+		add(rbRestoreSoft, c);
 		c.gridy++;
-		add(rbRestoreModified, c);
+		add(lbRestoreSoft, c);
+		c.gridy++;
+		add(new JSeparator(), c);
 		c.gridy++;
 		add(rbRestoreAll, c);
 		c.gridy++;
+		add(lbRestoreAll, c);
+		c.gridy++;
 		add(new JSeparator(), c);
+		c.gridy++;
+		add(rbUndoChanges, c);
+		c.gridy++;
+		add(lbUndoChanges, c);
 		c.gridy++;
 		add(ckDeleteNew, c);
 	}
@@ -71,12 +105,26 @@ public class RestorationModePanel extends JPanel {
 	public final int getMode() {
 		int mode = -1;
 		if (rbRestoreAll.isSelected()) {
-			mode = RESTORE_ALL;
-		} else if (rbRestoreDamaged.isSelected()) {
-			mode = RESTORE_DAMAGED;
-		} else if (rbRestoreModified.isSelected()) {
-			mode = RESTORE_MODIFIED;
+			mode = MODE_ALL;
+		} else if (rbRestoreSoft.isSelected()) {
+			mode = MODE_SOFT;
+		} else if (rbUndoChanges.isSelected()) {
+			mode = MODE_UNDO_CHANGES;
 		}
 		return mode;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() instanceof JRadioButton) {
+			if (((JRadioButton) e.getSource()).isSelected()) {
+				if (e.getSource() == rbRestoreSoft) {
+					ckDeleteNew.setSelected(false);
+					ckDeleteNew.setEnabled(false);
+				} else if (e.getSource() == rbUndoChanges || e.getSource() == rbRestoreAll) {
+					ckDeleteNew.setEnabled(true);
+				}
+			}
+		}
 	}
 }
