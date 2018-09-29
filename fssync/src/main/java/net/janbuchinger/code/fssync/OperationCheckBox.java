1127affcf65ed84d6a9a11e07cc81c49ab61731e
Jan Buchinger <janbuchinger@protonmail.ch>
2018-09-29 19:55:13 +0200

    added license header

--- fssync/src/main/java/net/janbuchinger/code/fssync/OperationCheckBox.java
+++ fssync/src/main/java/net/janbuchinger/code/fssync/OperationCheckBox.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2018 Jan Buchinger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.janbuchinger.code.fssync;
 
 import java.awt.event.ActionEvent;
@@ -13,6 +28,7 @@ public class OperationCheckBox extends JCheckBox implements ActionListener {
 		super("", operation.isSelected());
 		this.operation = operation;
 		addActionListener(this);
+		
 	}
 
 	@Override
@@ -23,9 +39,10 @@ public class OperationCheckBox extends JCheckBox implements ActionListener {
 
 	@Override
 	public void setEnabled(boolean b) {
-		super.setEnabled(b);
-		if (!b)
+		if (!b) {
 			setSelected(b);
+		}
+		super.setEnabled(b);
 	}
 
 	@Override
